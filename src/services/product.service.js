9c9a3da25827937ab86a34bdcb362ebb2193a672
nirbhayjakhar3196 <jakharnirbhay0000@gmail.com>
2026-08-02 22:26:48 +0530

    feat:Added product logic

---
+++ src/services/product.service.js
@@ -0,0 +1,54 @@
+import { getAllProduct, createProduct , getProductById , updateProduct, deleteProduct, findProductBySerial } from "@/repositories/product.repository";
+
+export async function createProductService(data , adminId){
+
+    const existingUser = await findProductBySerial(data.serialNumber)
+
+    if(existingUser){
+        throw new Error("Product with this serial number already exist")
+    }
+
+    const product = await createProduct({
+        ...data,
+        adminId
+    })
+
+    return product
+}
+
+export async function getAllProductService() {
+    return await getAllProduct();
+}
+
+export async function getProductByIdService(id) {
+    
+    const product = await getProductById(id)
+
+    if(!product){
+        throw new Error("Product not found");
+    }
+
+    return product;
+}
+
+export async function updateProductService(id , data){
+
+    const product = await getProductById(id)
+
+    if(!product){
+        throw new Error("Product not found");
+    }
+
+    return await updateProduct(id , data)
+}
+
+export async function deleteProductService(id) {
+    
+    const product = await getProductById(id)
+
+    if(!product){
+        throw new Error("Product not found");
+    }
+
+    return await deleteProduct(id)
+}
